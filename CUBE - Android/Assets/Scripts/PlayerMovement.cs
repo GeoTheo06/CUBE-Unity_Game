4e99fd1a2364ffe5f57be90d5210eba1387c616c
GeoTheo06 <theodorgeor@gmail.com>
2022-06-28 02:00:27 +0300

    deleted joystick and added 2 buttons for movement

--- CUBE - Android/Assets/Scripts/PlayerMovement.cs
+++ CUBE - Android/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour {
 	public Rigidbody rb;
@@ -32,35 +32,81 @@ public class PlayerMovement : MonoBehaviour {
 		timer = 0;
 		continuousForce = 2500;
 		horizontalControl = 0.5f;
+		i = 1;
+		i2 = 0;
 	}
 
 	private void Update() {
 		horizontalMoveForce = PlayerPrefs.GetInt("horizontalControlForce");
+
+		if (Input.touchCount == 1) {
+			if (Input.GetTouch(0).phase == TouchPhase.Began) {
+				Touch touch = Input.GetTouch(0);
+
+				//if not touching UI
+				if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId)) {
+					clicked = true;
+				}
+			}
+		}
+
+		//if they're touching the ui while pressing to jump
+		if (Input.touchCount == 2) {
+			if (Input.GetTouch(1).phase == TouchPhase.Began) {
+				Touch touch = Input.GetTouch(1);
+				clicked = true;
+			}
+		}
 	}
 
 	public GameManager1 gameManager;
+	bool goRight;
+	bool goLeft;
+
+	public void right() {
+		goRight = true;
+		goLeft = false;
+	}
+	public void left() {
+		goLeft = true;
+		goRight = false;
+	}
+	public void leftUp() {
+		goLeft = false;
+	}
+	public void rightUp() {
+		goRight = false;
+	}
 
 	public Joystick joystick;
+	float i;
+	float i2;
 	public void FixedUpdate() //using FixedUpdate instead of Update to make physics smoother
 	{
-
+		continuousForce += 0.0001f;
 		//this force is the one that makes the character go forward.
 		rb.AddForce(continuousForce * Time.deltaTime, 0, 0);
 
 
 		//Make character go left, right based on input
-		if (joystick.Horizontal <= -0.7f) {
+		if (goLeft && !goRight) {
 			player.position = new Vector3(player.position.x, player.position.y, player.position.z + horizontalControl);
 		}
-		if (joystick.Horizontal >= 0.7f) {
+		if (goRight && !goLeft) {
 			player.position = new Vector3(player.position.x, player.position.y, player.position.z - horizontalControl);
+		}
+
+		//simulate gravity
+		if (player.position.y < 2) {
+			player.position = new Vector3(player.position.x, player.position.y - 0.5f, player.position.z);
 		}
 
 		//jump
 		if (jump == true) {
 
 			if (!hasReachedMaxHeight) {
-				player.position = new Vector3(player.position.x, player.position.y + 1, player.position.z);
+				player.position = new Vector3(player.position.x, player.position.y + i, player.position.z);
+				i -= 0.03f;
 			} else {
 				if (remainIdle) {
 					rb.useGravity = false;
@@ -69,14 +115,16 @@ public class PlayerMovement : MonoBehaviour {
 						timer += Time.deltaTime;
 					}
 
-					if (timer > 0.5) {
+					if (timer > 0.25) {
 						stopCounting = true;
 						remainIdle = false;
 					}
 
 				} else {
 					rb.useGravity = true;
-					player.position = new Vector3(player.position.x, player.position.y - 0.2f, player.position.z);
+					player.position = new Vector3(player.position.x, player.position.y - i2, player.position.z);
+					i2 += 0.03f;
+
 
 					if (playerIsOnFloor) {
 						playerIsOnFloor = false;
@@ -85,6 +133,8 @@ public class PlayerMovement : MonoBehaviour {
 						remainIdle = true;
 						stopCounting = false;
 						timer = 0;
+						i = 1;
+						i2 = 0;
 					}
 				}
 			}
@@ -96,9 +146,6 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	public bool clicked = false;
-	public void onClick() {
-		clicked = true;
-	}
 
 	//make character jump on space bar
 	void OnCollisionStay(Collision collisionInfo) {
